dcb4176cc56e4992ae5913dc5e690b7153d64b07
Patrick Borowy <patrick@exordelabs.com>
2023-04-27 10:16:33 +0200

    dev: small adjustments

--- articles.sh
+++ articles.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-# Get the 7 most recent issues labeled with "article"
-issues=$(curl -s "https://api.github.com/repos/6r17/6r17/issues?labels=article&state=open&sort=created&direction=desc&page=1&per_page=7")
+# Retrieve the 7 most recent issues labeled with "article" and save the output to a file
+curl -s "https://api.github.com/repos/6r17/6r17/issues?labels=article&state=open&sort=created&direction=desc&page=1&per_page=7" > issues.json
 
-# Parse the JSON response to get the issue titles and URLs
-titles=$(echo "$issues" | jq -r '.[] | "- [\(.title)](\(.html_url))"')
+# Parse the JSON file to get the issue titles and URLs
+titles=$(cat issues.json | jq -r '.[] | "- [\(.title)](\(.html_url))"')
 
 # Combine BASE.md and the latest articles into README.md
-rm -rf README.md
-cat BASE.md > README.md
+rm README.md
+cat BASE.md >> README.md
 echo "$titles" >> README.md
-
+rm issues.json
